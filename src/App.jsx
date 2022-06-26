f51474d6565b3a473794976093c7f951c24b8e2e
Paulo Henrique <phvcandido@gmail.com>
2022-06-26 13:06:29 -0300

    Adicionando material ui e cypress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Typography } from "@mui/material";
+
 const App = () => {
-  return <h1>Olá Dev</h1>;
+  return (
+    <Typography variant="h2" component="h2" data-test="heading">
+      Olá Dev
+    </Typography>
+  );
 };
 
 export default App;
